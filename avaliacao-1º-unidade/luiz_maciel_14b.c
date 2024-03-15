8e8fe15143be456c270dd65deaa84b75707e8969
Luiz Carlos Oliveira Maciel <cgti@NOTE-MT08.belojardim.ifpe>
2024-03-15 13:45:45 -0300

    feat: question 14h finalized

--- avaliacao-1º-unidade/luiz_maciel_14b.c
+++ avaliacao-1º-unidade/luiz_maciel_14b.c
@@ -14,31 +14,47 @@
 // TAM MAX String
 #define MAX 100
 
+// Função preencher o vector com 0
+void fill_vector(char* string) {
+    for(int k = 0; k < MAX; k++) {
+        *(string+k) = 0;
+    }
+}
+
 // Contabilizar TAM da string
-int str_len(char string[]) {
+int str_len(char* string) {
     int cont = 0;
 
     for(int i = 0; i < MAX; i++) {
-        if (string[i] == 0) break;
+        if (*(string+i) == 0) break;
         cont++;
     }
     return cont;
 }
 
-void print_array(char string[], int tam_string) {
+void print_array(char* string, int tam_string) {
     for(int j = (tam_string - 1); j >= 0; j--) {
-        printf("%c", string[j]);
+        printf("%c", *(string+j));
     }
 }
 
 int main() {
-    char string[MAX] = {0};
+    char* string;
+
+    string = (char*) malloc(sizeof(char) * MAX);
+
+    if (string == NULL) {
+        printf("Erro na alocação de memória.");
+        return EXIT_FAILURE;
+    }
+
+    fill_vector(string);
 
     scanf("%s", string);
 
-    int tam_string = str_len(string);
+    print_array(string, str_len(string));
 
-    print_array(string, tam_string);
+    free(string);
 
     return EXIT_SUCCESS;
 }
