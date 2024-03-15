fbf04bcf77160885d89875db5ce41b63c1024581
luizcarlosom <lcom@discente.ifpe.edu.br>
2024-03-15 11:56:22 -0300

    feat: questions 12 and 13 completed

--- avaliacao-1º-unidade/luiz_maciel_14a.c
+++ avaliacao-1º-unidade/luiz_maciel_14a.c
@@ -25,6 +25,18 @@ int str_len(char string[]) {
 }
 
 void string_reverse(char string[], int tam_string) {
+    int cont = (tam_string - 1);
+
+    for(int j = 0; j < tam_string; j++) {
+        string[j] = string[cont];
+        cont--; 
+    }
+}
+
+void print_array(char string[], int tam_string) {
+    for(int k = 0; k < tam_string; k++) {
+        printf("%c", string[k]);
+    }
 }
 
 int main() {
@@ -32,7 +44,11 @@ int main() {
 
     scanf("%s", string);
 
-    printf("%d", str_len(string));
+    int tam_string = str_len(string);
+
+    string_reverse(string, tam_string);
+
+    print_array(string, tam_string);
 
     return EXIT_SUCCESS;
 }
